d82543c759bfe1dbe2a8296e8369b18cd3658f13
pavansreddy27 <pavansreddy26@gmail.com>
2025-01-30 00:17:40 +0530

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,30 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    if ($name === false) {
+        $name = "";
+    }
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = "pavansreddy26@gmail.com"; // Your email address
+    $subject = "Message from Portfolio Website";
+
+    $body = "You have received a new message from your portfolio website:\n\n";
+    $body .= "Name: " . $name . "\n\n";
+    $body .= "Email: " . $email . "\n\n";
+    $body .= "Message:\n" . $message;
+
+    $headers = "From: " . $name . " <" . $email . ">\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+
+    if (mail($to, $subject, $body, $headers)) {
+        header("Location: thank_you.html"); // Redirect to thank you page
+        exit();
+    } else {
+        echo "Sorry, there was an error sending your message. Please try again.";
+    }
+}
+
+?>
